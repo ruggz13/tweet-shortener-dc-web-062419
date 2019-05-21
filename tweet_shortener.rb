01c3d2bc6f3ded573b73a4432ebdb6be6b2cb1f3
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-21 20:40:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,3 +36,10 @@ def selective_tweet_shortener(tweet)
     tweet
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0...136] + "..."
+  else
+    tweet
+end
